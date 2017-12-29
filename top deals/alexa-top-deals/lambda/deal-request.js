3a5440186a115ced74036b28e1719efd43d31e6e
madhurgupta10 <madhurgupta10@gmail.com>
2017-12-29 20:32:41 +0530

    Add Remote

---
+++ top deals/alexa-top-deals/lambda/deal-request.js
@@ -0,0 +1,40 @@
+var http = require('http');
+var fs = require('fs');
+function finder(data) {
+    dealer("top");
+    fs.readFile('mydeals.txt', 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        data = JSON.parse(data);
+        data = data;
+        data = data['deal'][0]['deal'];
+        answer = data;
+        //return answer;
+        // console.log(data);
+    });
+}
+
+function dealer(query) {
+    var options = {
+        host: 'top-deals-api.herokuapp.com',
+        path: '/' + query
+    };
+
+    var req = http.get(options, function (res) {
+        var bodyChunks = [];
+        res.on('data', function (chunk) {
+            bodyChunks.push(chunk);
+        }).on('end', function (body) {
+            var body = Buffer.concat(bodyChunks);
+            body = "" + body;
+            fs.writeFile('mydeals.txt', body, function (err) {
+                if (err) throw err;
+                console.log('Saved!');
+            });
+        })
+    });
+}
+finder("top");
+setTimeout(function () { var h = answer; console.log(h) }, 1000);
+console.log(h);
---
+++ top deals/alexa-top-deals/lambda/index.js
@@ -0,0 +1,146 @@
+"use strict";
+var Alexa = require("alexa-sdk");
+var request = require('request');
+var SKILL_NAME = "Top Deals";
+var APP_ID = undefined;
+
+function dealer(contextWord, _callback) {
+    var options = {
+        url: 'https://top-deals-api.herokuapp.com/' + contextWord
+    };
+
+    request(options, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            //Get the info
+            var info = JSON.parse(body);
+            info = info['deal'];
+            var deals = [];
+            var links = [];
+            if (info) {
+                for (var i = 0; i < 5; i++) {
+                    deals.push(info[i].deal);
+                    links.push(info[i].link);
+                }
+                if (_callback) {
+                    var randIndex = Math.floor(Math.random() * (deals.length - 1));
+                    if (deals[randIndex]) {
+                        return _callback([deals, links]);
+                    }
+                }
+            }
+        }
+        else {
+            console.log("Error making request: " + error);
+        }
+        if (_callback) {
+            return _callback('');
+        }
+    });
+}
+
+exports.handler = function(event, context, callback){
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+}
+
+var handlers = {
+    'LaunchRequest': function() {
+        this.emit('startGameIntent');
+    },
+
+    'startIntent': function() {
+        var prompt = "Hi, Welcome to Top Deals. I will make your new year shopping easy just ask me to find top five deals from flipkart or top five deals from amazon";
+        var reprompt = "Try saying. Alexa, find me the top five deals";
+        this.emit(":ask", prompt, reprompt);
+    },
+
+    'topDealsIntent': function () {
+        dealer("top", (speechOutput) => {
+            if (speechOutput == '') {
+                this.emit('Unhandled');
+            }
+            else {
+                var cardString = speechOutput[0][0] + "\n" + speechOutput[1][0] + "\n" + "\n" +
+                                speechOutput[0][1] + "\n" + speechOutput[1][1] + "\n" + "\n" +
+                                speechOutput[0][2] + "\n" + speechOutput[1][2] + "\n" + "\n" +
+                                speechOutput[0][3] + "\n" + speechOutput[1][3] + "\n" + "\n" +
+                                speechOutput[0][4] + "\n" + speechOutput[1][4] + "\n"
+                var speechString = speechOutput[0][0] + ",,,," +
+                                    speechOutput[0][1] + ",,,," +
+                                    speechOutput[0][2] + ",,,," +
+                                    speechOutput[0][3] + ",,,," +
+                                    speechOutput[0][4] + ",,,,"
+                this.emit(":tellWithCard", speechString, SKILL_NAME, cardString);
+            }
+        });
+    },
+
+    'topAmazonDealsIntent': function () {
+        dealer("search/Amazon", (speechOutput) => {
+            if (speechOutput == '') {
+                this.emit('Unhandled');
+            } else {
+                var cardString = speechOutput[0][0] + "\n" + speechOutput[1][0] + "\n" + "\n" +
+                    speechOutput[0][1] + "\n" + speechOutput[1][1] + "\n" + "\n" +
+                    speechOutput[0][2] + "\n" + speechOutput[1][2] + "\n" + "\n" +
+                    speechOutput[0][3] + "\n" + speechOutput[1][3] + "\n" + "\n" +
+                    speechOutput[0][4] + "\n" + speechOutput[1][4] + "\n"
+                var speechString = speechOutput[0][0] + ",,,," +
+                    speechOutput[0][1] + ",,,," +
+                    speechOutput[0][2] + ",,,," +
+                    speechOutput[0][3] + ",,,," +
+                    speechOutput[0][4] + ",,,,"
+                this.emit(":tellWithCard", speechString, SKILL_NAME, cardString);
+            }
+        });
+    },
+
+    'topFlipkartDealsIntent': function () {
+        dealer("search/Flipkart", (speechOutput) => {
+            if (speechOutput == '') {
+                this.emit('Unhandled');
+            } else {
+                var cardString = speechOutput[0][0] + "\n" + speechOutput[1][0] + "\n" + "\n" +
+                    speechOutput[0][1] + "\n" + speechOutput[1][1] + "\n" + "\n" +
+                    speechOutput[0][2] + "\n" + speechOutput[1][2] + "\n" + "\n" +
+                    speechOutput[0][3] + "\n" + speechOutput[1][3] + "\n" + "\n" +
+                    speechOutput[0][4] + "\n" + speechOutput[1][4] + "\n"
+                var speechString = speechOutput[0][0] + ",,,," +
+                    speechOutput[0][1] + ",,,," +
+                    speechOutput[0][2] + ",,,," +
+                    speechOutput[0][3] + ",,,," +
+                    speechOutput[0][4] + ",,,,"
+                this.emit(":tellWithCard", speechString, SKILL_NAME, cardString);
+            }
+        });
+    },
+
+    'AMAZON.HelpIntent': function() {
+        var prompt = "Try saying. Alexa, find me the top five deals on Amazon";
+        var reprompt = "Try saying. Alexa, find me the top five deals on Flipkart";
+        this.emit(":ask", prompt, reprompt);
+    },
+
+    'AMAZON.StopIntent': function() {
+        this.emit(':tell', "I wish you and your family a very happy new year, GoodBye!");
+        
+    },
+
+    'AMAZON.CancelIntent': function() {
+        this.emit(':tell', "I wish you and your family a very happy new year, GoodBye!");
+
+    },
+
+    'Unhandled': function () {
+        console.log("UNHANDLED");
+        dealer("top", (speechOutput) => {
+            if (speechOutput == '') {
+                this.emit('Unhandled');
+            } else {
+                this.emit(':tell', "Sorry, I am not able to find deals right now");
+            }
+        });
+    }
+}
