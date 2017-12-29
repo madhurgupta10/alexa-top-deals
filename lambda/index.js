88e6df29306a469924f2934dee61b4ee31ee9829
madhurgupta10 <madhurgupta10@gmail.com>
2017-12-29 18:56:19 +0530

    Bug Fixed

--- lambda/index.js
+++ lambda/index.js
@@ -63,10 +63,10 @@ var handlers = {
             }
             else {
                 //this.emit(':tell', speechOutput);
-                var cardString = speechOutput[0][0] + "\n" + speechOutput[1][0] + "\n" + "\n"
-                                speechOutput[0][1] + "\n" + speechOutput[1][1] + "\n" + "\n"
-                                speechOutput[0][2] + "\n" + speechOutput[1][2] + "\n" + "\n"
-                                speechOutput[0][3] + "\n" + speechOutput[1][3] + "\n" + "\n"
+                var cardString = speechOutput[0][0] + "\n" + speechOutput[1][0] + "\n" + "\n" +
+                                speechOutput[0][1] + "\n" + speechOutput[1][1] + "\n" + "\n" +
+                                speechOutput[0][2] + "\n" + speechOutput[1][2] + "\n" + "\n" +
+                                speechOutput[0][3] + "\n" + speechOutput[1][3] + "\n" + "\n" +
                                 speechOutput[0][4] + "\n" + speechOutput[1][4] + "\n"
                 var speechString = speechOutput[0][0] + ",,,," +
                                     speechOutput[0][1] + ",,,," +
